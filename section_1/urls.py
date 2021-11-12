3e2e4de04ee642078cad1dd7f0f85dd3ac498c37
Saurav singh rautela <sauravsinghrautela9412@gmail.com>
2021-11-12 13:22:49 +0530

    fits commit

--- section_1/urls.py
+++ section_1/urls.py
@@ -3,5 +3,5 @@ from .views import (
     Landing_page,
 )
 urlpatterns = [
-    path('home', Landing_page.as_view(), name = "landing_page"),
+    path('', Landing_page.as_view(), name = "landing_page"),
 ]
